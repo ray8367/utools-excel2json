74aa5cf37281eaab2e240c877612a3bfc5e0bcb8
lainbo <lainbo@foxmail.com>
2022-05-01 19:52:58 +0800

    chore: 构建工具添加

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,53 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import { ArcoResolver } from 'unplugin-vue-components/resolvers'
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split'
 import WindiCSS from 'vite-plugin-windicss'
-import Components from 'unplugin-vue-components/vite';
-import { ArcoResolver } from 'unplugin-vue-components/resolvers';
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     WindiCSS(),
+    AutoImport({
+      eslintrc: {
+        enabled: true,
+        filepath: './.eslintrc-auto-import.json',
+        globalsPropValue: true
+      },
+      imports: ['vue', '@vueuse/core']
+    }),
     Components({
-      resolvers: [
-        ArcoResolver()
-      ]
+      resolvers: [ArcoResolver()]
+    }),
+    chunkSplitPlugin({
+      strategy: 'default',
+      customSplitting: {
+        utils: [/src\/utils/],
+        assets: [/src\/assets/],
+        'component-library': ['@arco-design/web-vue']
+      }
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
+  build: {
+    assetsInlineLimit: 4096,
+    cssCodeSplit: true,
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  }
 })
