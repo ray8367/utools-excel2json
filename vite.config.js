e535aa216b72fce0371717277defdf79d6031f7f
raorui <raorui@ganweiiot.com.cn>
2022-05-01 17:21:43 +0800

    引入各开发工具库

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import WindiCSS from 'vite-plugin-windicss'
+import Components from 'unplugin-vue-components/vite';
+import { ArcoResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    WindiCSS(),
+    Components({
+      resolvers: [
+        ArcoResolver()
+      ]
+    })
+  ]
 })
