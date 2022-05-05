9667c444925744cd6835f3604e275baf00699f9b
liangjiacheng <lainbo@foxmail.com>
2022-05-05 16:20:06 +0800

    docs: 提交key文件

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const baidu = {
+  appid: '20190324000280623',
+  token: 'uXMeEILLbOwuPz5BbekJ'
+}
+
+export const tencent = {
+  secretId: 'AKIDTmpc8NL2ViBPM2RaS3NREkzyh9KEsFK8',
+  secretKey: 'Zrsc5IEsGOFo69vPcYCZfT1II0SdXA9t'
+}
