b8b0df5606ebf0c51e8360d00a472337df79b628
rui <raorui2020@foxmail.com>
2022-05-12 00:14:02 +0800

    refactor: 移除api/setting，改为pinia 来管理设置信息；二次封装数据存储，兼容utools和browser

--- src/store/userSetting.js
+++ src/store/userSetting.js
@@ -1,12 +1,35 @@
 import { defineStore } from 'pinia'
+import { homeOptionStorage, defaultStorage, keyStorage } from '@/utils/storage'
 
-export const useSettingStore = defineStore('settings', {
+export const userSettingStore = defineStore('settings', {
   state: () => {
     return {
-      name: '张三',
-      age: 18
+      // 首页显示的翻译接口
+      homeOption: homeOptionStorage.get(),
+      defaultApi: defaultStorage.get(),
+      keyConfig: keyStorage.get()
     }
   },
+
   getters: {},
-  actions: {}
+
+  actions: {
+    /**设置首页可见翻译 */
+    setHomeOption(data) {
+      this.homeOption = data
+      homeOptionStorage.set(data)
+    },
+
+    /** 设置默认翻译 */
+    setDefaultStorage(data) {
+      this.defaultApi = data
+      defaultStorage.set(data)
+    },
+
+    /** 设置密钥 */
+    setKeyConfig(data) {
+      this.keyConfig = data
+      keyStorage.set(data)
+    }
+  }
 })
--- src/utils/storage.js
+++ src/utils/storage.js
@@ -5,16 +5,37 @@ const KEY_SETTING = 'keyConfig'
 const HOME_OPTION = 'homeOption'
 const DEFAULT_API = 'defaultApi'
 
+function getDbStorageItem(key) {
+  if (utools) {
+    return utools.dbStorage.getItem(key)
+  } else {
+    return window.localStorage.getItem(key)
+  }
+}
+
+function setDbStorageItem(key, value) {
+  if (utools) {
+    return utools.dbStorage.setItem(key, value)
+  } else {
+    return window.localStorage.setItem(key, value)
+  }
+}
+
 // 密钥存储
 export const keyStorage = {
   /**获取密钥配置信息 */
   get() {
-    return utools.dbStorage.getItem(KEY_SETTING) || {}
+    const strData = getDbStorageItem(KEY_SETTING) || '{}'
+    try {
+      return JSON.parse(strData)
+    } catch (error) {
+      return {}
+    }
   },
 
   /**存储密钥配置信息 */
   set(data) {
-    utools.dbStorage.setItem(KEY_SETTING, data)
+    setDbStorageItem(KEY_SETTING, JSON.stringify(data))
   },
 
   /**
@@ -23,7 +44,6 @@ export const keyStorage = {
    */
   getKeyByTag(tag) {
     const keyConfig = this.get()
-    console.log('keyConfig:', keyConfig)
     return keyConfig[tag]
   },
 
@@ -31,28 +51,39 @@ export const keyStorage = {
   setKeyByTag(tag, data) {
     const keyConfig = this.get()
     keyConfig[tag] = data
-    utools.dbStorage.setItem(KEY_SETTING, keyConfig)
+    this.set(keyConfig)
   }
 }
 
 // 首页选项
 export const homeOptionStorage = {
   get() {
-    return utools.dbStorage.getItem(HOME_OPTION)
+    const defaultOptions = ['baidu', 'tencent', 'youdao', 'ali']
+    const strData = getDbStorageItem(HOME_OPTION)
+    // return strData ? JSON.parse(strData) : defaultOptions
+    if (strData) {
+      try {
+        return JSON.parse(strData)
+      } catch (error) {
+        return defaultOptions
+      }
+    } else {
+      return defaultOptions
+    }
   },
 
-  set() {
-    return utools.dbStorage.setItem(HOME_OPTION)
+  set(data) {
+    setDbStorageItem(HOME_OPTION, JSON.stringify(data))
   }
 }
 
 // 首选翻译
 export const defaultStorage = {
   get() {
-    return utools.dbStorage.getItem(DEFAULT_API)
+    return getDbStorageItem(DEFAULT_API) || 'baidu'
   },
 
-  set() {
-    return utools.dbStorage.setItem(DEFAULT_API)
+  set(data) {
+    setDbStorageItem(DEFAULT_API, data)
   }
 }
