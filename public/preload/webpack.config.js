5c1904da44e2f1ff6a51d9409b0d1c82f43d5e7d
rui <raorui2020@foxmail.com>
2022-05-14 13:42:50 +0800

    ci(): ➕ 修改prelaod打包配置，增加了clean-webpack-plugin依赖

--- public/preload/webpack.config.js
+++ public/preload/webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
+// 引入clean-webpack-plugin插件
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   target: 'electron-renderer',
   mode: 'production',
   entry: path.join(__dirname, './src/index.js'),
   output: {
-    path: path.join(__dirname, './'),
+    path: path.join(__dirname, './dist'),
     filename: 'index.js'
   },
 
@@ -14,5 +17,16 @@ module.exports = {
     alias: {
       crypto: false
     }
+  },
+
+  plugins: [new CleanWebpackPlugin()],
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false //不将注释提取到单独的文件中
+      })
+    ]
   }
 }
