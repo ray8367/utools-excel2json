3d2eecab3975501851dd3515deda96c815ac7541
lainbo <lainbo@foxmail.com>
2022-05-18 22:03:27 +0800

    fix: 修复loading组件css

--- src/components/loading.vue
+++ src/components/loading.vue
@@ -19,7 +19,7 @@
         </linearGradient>
       </defs>
       <circle
-        class="ap__ring"
+        class="ap_ring"
         r="56"
         cx="64"
         cy="192"
@@ -29,7 +29,7 @@
         stroke-linecap="round"
       />
       <circle
-        class="ap__worm1"
+        class="ap_worm1"
         r="56"
         cx="64"
         cy="192"
@@ -40,7 +40,7 @@
         stroke-dasharray="87.96 263.89"
       />
       <path
-        class="ap__worm2"
+        class="ap_worm2"
         d="M120,192A56,56,0,0,1,8,192C8,161.07,16,8,64,8S120,161.07,120,192Z"
         fill="none"
         stroke="url(#ap-grad2)"
@@ -55,4 +55,77 @@
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.ap {
+  width: 50px;
+  height: 100px;
+}
+
+.ap_ring {
+  stroke: #17181c26;
+  transition: stroke 0.3s;
+}
+
+.ap_worm1,
+.ap_worm2 {
+  animation-duration: 3s;
+  animation-iteration-count: infinite;
+}
+
+.ap_worm1 {
+  animation-name: worm1;
+}
+
+.ap_worm2 {
+  visibility: hidden;
+  animation-name: worm2;
+}
+
+@keyframes worm1 {
+  0% {
+    stroke-dashoffset: -87.96;
+    animation-timing-function: ease-in-out;
+  }
+
+  20% {
+    stroke-dashoffset: 0;
+    animation-timing-function: ease-in;
+  }
+
+  60% {
+    visibility: visible;
+    stroke-dashoffset: -791.68;
+  }
+
+  60.1%,
+  100% {
+    visibility: hidden;
+    stroke-dashoffset: -791.68;
+  }
+}
+
+@keyframes worm2 {
+  0%,
+  60% {
+    visibility: hidden;
+    stroke-dashoffset: -87.96;
+  }
+
+  60.1% {
+    visibility: visible;
+    stroke-dashoffset: -87.96;
+    animation-timing-function: cubic-bezier(0, 0, 0.5, 0.75);
+  }
+
+  77% {
+    visibility: visible;
+    stroke-dashoffset: -340;
+    animation-timing-function: cubic-bezier(0.5, 0.25, 0.5, 0.88);
+  }
+
+  100% {
+    visibility: visible;
+    stroke-dashoffset: -669.92;
+  }
+}
+</style>
