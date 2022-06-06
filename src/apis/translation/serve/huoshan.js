7d42cd5a95b45306909fd7a1b228c6508c845dc4
rui <raorui2020@foxmail.com>
2022-06-06 21:41:30 +0800

    火山翻译接口签名 - 测试中

--- src/apis/translation/serve/huoshan.js
+++ src/apis/translation/serve/huoshan.js
@@ -10,28 +10,27 @@ import { toResultData } from '../common'
 import dayjs from 'dayjs'
 
 const errors = {
-  100001: '一般不出现这个问题，这是系统开发兜底的错误提示',
-  100002: '关键参数缺失，例如Action, Version参数',
-  100003: '缺少身份认证的必要信息，例如Auth信息',
-  100004: '缺少请求必要信息，例如：Accesskey，service，region等',
-  100005: '缺少签名结果',
-  100006: '请求过期或请求的签名时间来自未来',
-  100007: '请求的服务不存在',
-  100008: '请求接口不存在',
-  100009: '请求的AK不合法',
-  100010: '签名结果不正确',
-  100011:
-    '使用service aksk访问请求，仅能访问自己的接口和公共服务的接口。公共服务例如：iam',
-  100012: '子用户请求缺少权限（默认是deny的）',
-  100013: '子用户拥有的权限不支持当前操作',
-  100014: '业务服务存在故障，这里的业务一般指具体的imagex、vod。故障不来自TOP',
-  100015: '业务服务连接不上，这里的业务一般指具体的imagex、vod。',
-  100016: '内部服务执行超时，请查看服务在TOP上配置的超时时间。',
-  100017: 'Service aksk请求内部服务时缺少了账号信息',
-  100018: '请求过于频繁，超出了基本限速',
-  100019: '处于熔断状态的服务暂时不可访问，稍后重试',
-  100020: 'Http method不合法',
-  100021: '请求需要验证Project的权限，当前Project权限验证不合法'
+  100002: '关键参数缺失，例如Action, Version参数。',
+  100003: '缺少身份认证的必要信息，例如Auth信息。',
+  100004: '缺少请求必要信息，例如：Access Key，Service，Region等。',
+  100005: '缺少签名结果。',
+  100006: '请求过期或请求的签名时间来自未来。',
+  100007: '请求的服务不存在。',
+  100008: '请求接口不存在。',
+  100009: '请求的Access Key不合法。',
+  100010: '签名结果不正确。',
+  100012: '子用户缺少权限。',
+  100013: '子用户拥有的权限不支持当前操作。',
+  100014: '内部错误。',
+  100015: '已通过身份认证，但服务无法连接。',
+  100016: '服务执行超时。',
+  100018: '请求过于频繁，超出了基本限速。',
+  100019: '处于熔断状态的服务暂时不可访问，稍后重试。',
+  100020: 'Http method不合法。',
+  100023: '服务存在故障。',
+  100024: 'Authorization头格式错误，检查Authorization。',
+  100025: 'Authorization头中的Credential格式错误，检查Credential。',
+  100026: '错误的STS or STS2，可能是多种错误，例如签名错误、过期等。'
 }
 
 /**
@@ -43,36 +42,31 @@ const errors = {
  */
 export default async function ({ q, from, to, keyConfig }) {
   const url = import.meta.env.VITE_HUOSHAN_BASEURL
-  const nowTime = dayjs()
-  const timestamp = nowTime.unix().toString()
-  // const date = nowTime.format('YYYY-MM-DD')
+
   const query = 'Action=TranslateText&Version=2020-06-01'
-  const params = {
+  const bodyData = {
     TextList: [q],
     SourceLanguage: from,
     TargetLanguage: to
   }
 
-  const authorization = toSign(query, params, keyConfig, timestamp, nowTime)
-  const headers = {
-    'Content-Type': 'application/json'
-  }
+  const headers = toSign(query, bodyData, keyConfig)
+
   try {
-    const res = await axios.post(url + `?${query}`, params, { headers })
-    const { TargetText, Error } = res.data?.Response
+    const res = await axios.post(url + `?${query}`, bodyData, { headers })
+    const { TargetText, Error } = res.data?.ResponseMetadata
     let result
-    if (Error) {
-      // 翻译失败
-      result = toResultData(500, null, errors[Error.Code])
+    const apiError = Error?.CodeN
+    if (apiError) {
+      return toResultData(500, null, errors[apiError])
     } else {
-      console.log('TargetText:', TargetText)
       result = toResultData(200, { text: TargetText })
     }
     return result
   } catch (err) {
     const apiError = err?.response?.data?.ResponseMetadata?.Error?.CodeN
+    console.log('apiError:', apiError)
     if (apiError) {
-      console.log('sss')
       return toResultData(500, null, errors[apiError])
     } else {
       console.error(err)
@@ -81,25 +75,48 @@ export default async function ({ q, from, to, keyConfig }) {
   }
 }
 
-/** 规范化header */
-function getCanonicalHeadersEntry(headerName, headerValue) {
-  let result = `${headerName}:${headerValue.trim()}\n`
-  return result.toLowerCase()
+/** 获取规范化header数组 */
+function getCanonicalHeadersArr(headers) {
+  let result = []
+  Object.keys(headers).forEach(key => {
+    result.push({
+      key: key.toLowerCase(),
+      value: headers[key]
+    })
+  })
+  result.sort((h1, h2) => {
+    return h1.key.charCodeAt() - h2.key.charCodeAt()
+  })
+  console.log(result)
+  return result
 }
 
 /** 生成签名 */
-function toSign(query, params, keyConfig, timestamp, nowTime) {
-  const payload = JSON.stringify(params)
-  const { secretId, secretKey } = keyConfig
+function toSign(query, bodyData, keyConfig) {
+  const payload = JSON.stringify(bodyData)
+  const { accessKeyId, secretAccessKey } = keyConfig
+  const nowTime = dayjs().add('-8', 'h')
+  const xDate = nowTime.format('YYYYMMDDTHHmmss[Z]')
+  const date = xDate.slice(0, 8)
+  const headers = {
+    'X-Host': 'open.volcengineapi.com',
+    'Content-Type': 'application/json; charset=utf-8',
+    'X-Content-Sha256': SHA256(payload).toString(encHex),
+    'X-Date': xDate
+  }
+  const headerArr = getCanonicalHeadersArr(headers)
+  const headerKeys = headerArr.map(item => item.key)
+  const region = 'cn-north-1'
+  const service = 'translate'
   /** 1. 创建一个正规化请求  */
   const HTTPRequestMethod = 'POST'
   const CanonicalURI = '/'
   const CanonicalQueryString = query
-  const CanonicalHeaders = getCanonicalHeadersEntry(
-    'Content-Type',
-    'application/json'
-  )
-  const SignedHeaders = 'content-type'
+  let CanonicalHeaders = ''
+  headerArr.forEach(item => {
+    CanonicalHeaders += `${item.key}:${item.value}\n`
+  })
+  const SignedHeaders = headerKeys.join(';')
 
   const CanonicalRequest =
     HTTPRequestMethod +
@@ -118,15 +135,34 @@ function toSign(query, params, keyConfig, timestamp, nowTime) {
 
   /** 2. TODO:创建签名字符串 */
   console.log('2. 创建签名字符串')
+  const Algorithm = 'HMAC-SHA256'
+  const RequestDate = xDate
+  const CredentialScope = `${date}/${region}/${service}/request`
   const StringToSign =
-    'HMAC-SHA256' +
+    Algorithm +
     '\n' +
-    nowTime.format('YYYYMMDDTHHMMSSZ') +
+    RequestDate +
     '\n' +
-    nowTime.format('YYYYMMDD') +
-    '/region/service/request' +
+    CredentialScope +
     '\n' +
     // HexEncode(Hash(CanonicalRequest))
     SHA256(CanonicalRequest).toString(encHex)
   console.log(StringToSign)
+
+  /** 3：构建签名 */
+  console.log('3：构建签名')
+  // const Signingkey = HMAC(HMAC(HMAC(HMAC(kSecret,"20201230"),"cn-north-1"),"iam"),"request");
+  const signingkey_temp1 = hmacSHA256(date, secretAccessKey)
+  const signingkey_temp2 = hmacSHA256(region, signingkey_temp1)
+  const signingkey_temp3 = hmacSHA256(service, signingkey_temp2)
+  const Signingkey = hmacSHA256('request', signingkey_temp3)
+  const signature = hmacSHA256(StringToSign, Signingkey).toString(encHex)
+  console.log(signature)
+  /** 4：将签名添加到请求当中 */
+  console.log('4：将签名添加到请求当中')
+  const authorization = `HMAC-SHA256 Credential=${accessKeyId}/${CredentialScope}, SignedHeaders=${SignedHeaders}, Signature=${signature}`
+  console.log('authorization:', authorization)
+  headers.Authorization = authorization
+  console.log(headers)
+  return headers
 }
