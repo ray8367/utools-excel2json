492bb3da2b91e40fbe639fe3b14e0e9c2baaf1bb
liangjiacheng <lainbo@foxmail.com>
2022-06-15 17:18:57 +0800

    feat: 切换主题组件

---
+++ src/components/darkFullPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bg-black fixed w-screen h-screen grid-c">
+    <div class="moon"></div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.moon {
+  @apply relative w-200px h-200px rounded-full;
+  border-top: 5px solid #fff;
+  transform: rotate(60deg);
+  filter: drop-shadow(0 0 2px #fff) drop-shadow(0 0 5px #fff)
+    drop-shadow(0 0 10px #fff) drop-shadow(0 0 20px #fff);
+  animation: rotate 0.8s $imitate-ios 0.2s both;
+}
+
+@keyframes rotate {
+  100% {
+    transform: rotate(-70deg);
+  }
+}
+</style>
---
+++ src/components/lightFullPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="bg_wrapper fixed w-screen h-screen grid-c relative overflow-hidden"
+  >
+    <div class="relative w-200px h-200px">
+      <div class="sun"></div>
+      <div class="sun_ray"></div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.bg_wrapper {
+  background: linear-gradient(0deg, #62a0d8 0%, #2178d1 50%, #085cb3 100%);
+}
+.sun {
+  @apply absolute z-1 w-full h-full rounded-full bg-[#ffcd47];
+  box-shadow: 0 0 50px #ffff77;
+  animation: sun 0.8s ease 0.5s both;
+}
+.sun_ray {
+  @apply w-full h-full rounded-full bg-white opacity-50 relative z-0;
+  animation: sun-ray 0.8s ease 0.2s both;
+}
+
+@keyframes sun {
+  50% {
+    box-shadow: 0 0 100px #ffff77, 0 0 100px #ffff77, 0 0 100px #ffff77,
+      0 0 100px #ffff77;
+  }
+}
+@keyframes sun-ray {
+  100% {
+    transform: scale(15);
+    opacity: 0;
+  }
+}
+</style>
