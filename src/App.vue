bdf573cbd3b4523ee6456ace4cd25adfde25f06c
liangjiacheng <lainbo@foxmail.com>
2022-06-15 17:34:48 +0800

    feat: 根据主题切换动画50%

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
+  <!-- <component :is="comName" /> -->
   <MainView />
 </template>
 
 <script setup>
 import MainView from '@/views/MainView.vue'
+import LightFullPage from '@/components/lightFullPage.vue'
+import DarkFullPage from '@/components/darkFullPage.vue'
 // import MainView from '@/components/Test.vue'
 import '@/utils/setTheme.js'
+const comName = ref(MainView)
+const isDark = useDark() // 响应式：是否为暗色
+// watch(isDark, val => {
+//   if (val) {
+//     comName.value = DarkFullPage
+//   } else {
+//     comName.value = LightFullPage
+//   }
+// })
 </script>
 
 <style></style>
--- src/utils/setTheme.js
+++ src/utils/setTheme.js
@@ -5,7 +5,7 @@ const isDark = useDark() // 响应式：是否为暗色
  * 设置主题
  * @param {Boolean} val true: 深色，false: 浅色
  */
-export function setTheme(val) {
+export function setTheme (val) {
   document.documentElement.classList.toggle('dark', val)
   val
     ? bodyDom.setAttribute('arco-theme', 'dark')
