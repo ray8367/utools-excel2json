9c7217704e30029fe2b8bcd9a2d731c7299acb0c
liangjiacheng <lainbo@foxmail.com>
2022-07-06 17:47:20 +0800

    docs: 加TODO

--- src/utils/setTheme.js
+++ src/utils/setTheme.js
@@ -5,6 +5,7 @@ const bodyDom = document.body // body的dom
  * @param {String} val 'dark': 深色，'light': 浅色
  */
 export function setTheme (val) {
+  // TODO:把这个val设置到store里面
   const 是否黑色 = val === 'dark'
   document.documentElement.classList.toggle('dark', 是否黑色)
   val === 'dark'
