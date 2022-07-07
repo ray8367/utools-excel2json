22b0e811638eb6bbdbb2cd6cf1f7ebb6dd98dde4
lainbo <lainbo@foxmail.com>
2022-07-07 19:18:19 +0800

    chore: 升级依赖

--- src/views/useUtools.js
+++ src/views/useUtools.js
@@ -8,7 +8,7 @@ export default function (设置弹框Ref, 用户输入, 改变命名模式类型
   const { currentOS } = storeToRefs(globalStore)
 
   // 初始化utools
-  function utools初始化 () {
+  function utools初始化() {
     utools.onPluginEnter(({ code, payload }) => {
       设置弹框Ref.value.关闭弹窗()
       let value = ''
@@ -22,14 +22,14 @@ export default function (设置弹框Ref, 用户输入, 改变命名模式类型
   }
 
   // 粘贴
-  async function 粘贴 () {
+  async function 粘贴() {
     if (!utools) return
     const key = currentOS.value === 'MacOS' ? 'command' : 'ctrl'
     await utools.simulateKeyboardTap('v', key)
   }
 
   // 延迟时间关闭utools
-  function 延迟关闭utools (delayTime = 300) {
+  function 延迟关闭utools(delayTime = 300) {
     if (!utools) return
     return new Promise(resolve => {
       延迟(function () {
