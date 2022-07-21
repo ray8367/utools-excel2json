db4f993e6e6e14513b77c28ab218cb8e8bcbf64e
liangjiacheng <lainbo@foxmail.com>
2022-07-21 21:02:45 +0800

    docs: 添加注释

--- unocss.config.js
+++ unocss.config.js
@@ -4,20 +4,20 @@ import {
   presetIcons,
   presetUno,
   transformerDirectives,
-  transformerVariantGroup,
-} from "unocss";
-import presetRemToPx from "@unocss/preset-rem-to-px";
+  transformerVariantGroup
+} from 'unocss'
+import presetRemToPx from '@unocss/preset-rem-to-px'
 
 export default defineConfig({
   shortcuts: [
     {
-      "flex-c": "flex items-center justify-center",
-      "grid-c": "grid place-items-center",
-      "absolute-x-center": "absolute left-1/2 -translate-x-1/2",
-      "absolute-y-center": "absolute top-1/2 -translate-y-1/2",
-      "absolute-center":
-        "absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2",
-    },
+      'flex-c': 'flex items-center justify-center',
+      'grid-c': 'grid place-items-center',
+      'absolute-x-center': 'absolute left-1/2 -translate-x-1/2',
+      'absolute-y-center': 'absolute top-1/2 -translate-y-1/2',
+      'absolute-center':
+        'absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2'
+    }
   ],
   presets: [
     presetUno(),
@@ -26,32 +26,33 @@ export default defineConfig({
     presetIcons({
       warn: true,
       extraProperties: {
-        display: "inline-block",
-        "vertical-align": "middle",
-        "margin-top": "-1px",
-      },
-    }),
+        display: 'inline-block',
+        'vertical-align': 'middle',
+        'margin-top': '-1px'
+      }
+    })
   ],
   theme: {
     colors: {
-      primary: "#165dff", // 浅色主色的颜色值
-      darkPrimary: "#f7ba2a", // 深色主色的颜色值
-    },
+      primary: '#165dff', // 浅色主色的颜色值
+      darkPrimary: '#f7ba2a' // 深色主色的颜色值
+    }
   },
   transformers: [transformerDirectives(), transformerVariantGroup()],
   variants: [
-    // 为important写一种变体，让用"I_"开头的class也是important，兼容scss文件
-    (matcher) => {
-      if (!matcher.startsWith("I_")) return matcher;
+    // 因为.scss文件里面,uno不能写"!xxx"去表示 !importannt ,会报错
+    // 这里为important写一种变体，让以"I_"开头的class也是important，兼容scss文件
+    matcher => {
+      if (!matcher.startsWith('I_')) return matcher
       return {
         matcher: matcher.slice(2),
-        body: (body) => {
-          body.forEach((v) => {
-            if (v[1]) v[1] += " !important";
-          });
-          return body;
-        },
-      };
-    },
-  ],
-});
+        body: body => {
+          body.forEach(v => {
+            if (v[1]) v[1] += ' !important'
+          })
+          return body
+        }
+      }
+    }
+  ]
+})
